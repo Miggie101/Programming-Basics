6e25d4d3663e469feee1af5d1a530e20a07e9dd5
Miggie101 <126221305+Miggie101@users.noreply.github.com>
2023-02-23 19:47:57 -0800

    Add files via upload

---
+++ lab 2 completed.py
@@ -0,0 +1,34 @@
+# Miguel Madrigal
+# Feb. 2023
+# Lab 2
+
+# This program will show us numbers raised to the power of 1-8 with the base number being 2
+
+
+# Program Greeting
+print('Welcome to the Power of 2 program!!')
+print()
+
+# Calculations
+an1 = 2 ** 1
+an2 = 2 ** 2
+an3 = 2 ** 3
+an4 = 2 ** 4
+an5 = 2 ** 5
+an6 = 2 ** 6
+an7 = 2 ** 7
+an8 = 2 ** 8
+
+
+# display answers
+print('2 raised to the power of 1 equals', an1)
+print('2 raised to the power of 2 equals', an2)
+print('2 raised to the power of 3 equals', an3)
+print('2 raised to the power of 4 equals', an4)
+print('2 raised to the power of 5 equals', an5)
+print('2 raised to the power of 6 equals', an6)
+print('2 raised to the power of 7 equals', an7)
+print('2 raised to the power of 8 equals', an8)
+
+print()
+print('Thank you, have a nice day!!')
