47ccf524ed56046ca43a8cfb3f1cd4108eca4411
Miggie101 <126221305+Miggie101@users.noreply.github.com>
2023-02-23 19:51:18 -0800

    Update and rename Lab 3 completed.py to Lab-3.py

--- Lab-3.py
+++ Lab-3.py
@@ -4,7 +4,6 @@
 
 # This program will let the user raise any integer to any power
 
-
 # user greeting
 print('This program will let you raise any interger to any interger power.')
 print('Hello user, Welcome to Power Caculator')
@@ -12,31 +11,21 @@ print('Remeber to only use WHOLE numbers!!')
 print()
 
 # base number
-base = int(input('Please enter the base number: '))
+base = input('Please enter the base number: ')
 
 # exponent number
-exponent = int(input('Please enter the power number for ' + str(base) + ': '))
+exponent = int(input('Please enter the power number for ' + base + ': '))
 
 # configure base into a integer
-# base = int(base)
+base = int(base)
 print()
-
 
 # calculation raising the power with the base
 answer = base ** exponent
 
-
 # user final answer as a sentence
 print(base, 'raised to the power of', exponent, 'equals', format(answer, ','))
 print()
-
-
 # exit message
-print('Thank you for using my Power Calculator,\nHave a nice day')
-# print('Have a nice day :)')
-
-
-
-
-
-        
+print('Thank you for using my Power Calculator,')
+print('Have a nice day :)')
