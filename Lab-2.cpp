3f79900b3a72bfb04d41a2eb1862729a77e68e2d
Miggie101 <126221305+Miggie101@users.noreply.github.com>
2023-03-01 14:30:30 -0800

    Create Lab-2.cpp

---
+++ Lab-2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+int main(){
+	cout << "Welcome to the Power of 2 Program\n\n";
+
+	double an1 = pow(2, 1);
+	double an2 = pow(2, 2);
+	double an3 = pow(2, 3);
+	double an4 = pow(2, 4);
+	double an5 = pow(2, 5);
+	double an6 = pow(2, 6);
+	double an7 = pow(2, 7);
+	double an8 = pow(2, 8);
+
+	cout << "2 raised to the power of 1 equals " << fixed << an1 << endl;
+	cout << "2 raised to the power of 2 equals " << fixed << an2 << endl;
+	cout << "2 raised to the power of 3 equals " << fixed << an3 << endl;
+	cout << "2 raised to the power of 4 equals " << fixed << an4 << endl;
+	cout << "2 raised to the power of 5 equals " << fixed << an5 << endl;
+	cout << "2 raised to the power of 6 equals " << fixed << an6 << endl;
+	cout << "2 raised to the power of 7 equals " << fixed << an7 << endl;
+	cout << "2 raised to the power of 8 equals " << fixed << an8 << endl;
+
+	cout << "\nThank you, have a nice day!!";
+
+	return 0;
+} 
