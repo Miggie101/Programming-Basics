32b686fc1b5ebe3cb2be20fc3826e7b0aea5d3b5
Miggie101 <126221305+Miggie101@users.noreply.github.com>
2023-03-03 15:35:00 -0800

    Create Lab-3.cpp

---
+++ Lab-3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	int base;
+	int exponent;
+
+   cout << "This program will let the user raise any integer to any power\n";
+   cout << "Hello user. Welcome to the Power Caculator!\n";
+   cout << "Remeber to use WHOLE numbers\n";
+
+   cout << "\nPlease enter the base number: ";
+   cin >> base;
+
+   cout << "Please enter the power number for " << base << ": ";
+   cin >> exponent;
+
+   double answer = pow(base, exponent);
+
+   cout << "\n" << base << " raised to the power of " << exponent << " equals " << int(answer) << "\n";
+
+   cout << "\nHave a nice day!! :)\n";
+
+   return 0;
+}
